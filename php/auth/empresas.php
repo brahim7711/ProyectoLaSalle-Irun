6cc0a614f839ac2d6ba07154b07ffe4ae9cdcc33
Juan Antonio Alfro Peña <xbluuz@gmail.com>
2025-12-11 13:55:42 +0100

    Update

--- php/auth/empresas.php
+++ php/auth/empresas.php
@@ -13,14 +13,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['check_email'])) {
         $stmt->execute();
         $result = $stmt->get_result();
         $count = $result->num_rows;
+        $empresa_autorizada = $result->fetch_assoc();
         $stmt->close();
 
-        if ($count > 0) {
-            $auth_message = "<p class='success-message'>El correo electrónico está autorizado. Puede proceder con el registro.</p>";
-            header("Location: ./registro_empresas.php?correo=" . urlencode($correo));
-            exit();
-        } else {
-            $auth_message = "<p class='error-message'>El correo electrónico no está autorizado.</p>";
+        if ($empresa_autorizada) {
+            $stmt = $conexion->prepare("SELECT * FROM empresas WHERE id_autorizado=? LIMIT 1");
+            $stmt->bind_param('i', $empresa_autorizada['id_autorizado']);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $count2 = $result->num_rows;
+            $stmt->close();
+        }
+
+        if ($count2 > 0) {
+            $auth_message = "<p class='error-message'>El correo electrónico ya está registrado.</p>";
+        } else{
+            if ($count > 0) {
+                $auth_message = "<p class='success-message'>El correo electrónico está autorizado. Puede proceder con el registro.</p>";
+                header("Location: ./registro_empresas.php?correo=" . urlencode($correo));
+                exit();
+            } else {
+                $auth_message = "<p class='error-message'>El correo electrónico no está autorizado.</p>";
+            }
         }
     }
 }
